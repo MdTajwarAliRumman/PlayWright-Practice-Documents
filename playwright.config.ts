9ea0ef1a149dfee502726e4e8b14ddb87f5e9bbc
MdTajwarAliRumman <tajwaralirumman@gmail.com>
2026-01-19 16:11:08 +0600

    Json_Junit_dot_html_test_Reports

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
 import { on } from 'events';
+import { json } from 'stream/consumers';
 
 /**
  * Read environment variables from file.
@@ -32,7 +33,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'],
+  // ['list'],
+  ['dot'],
+  ['json', { outputFile: 'json-test-report.json' }],
+  ['junit', { outputFile: 'junit-test-report.xml' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
